88205a81afb3da96a7f25e13db87c6c4e08d7a3c
libiao <libiao2014@xiaochuankeji.cn>
2022-05-21 18:10:50 +0800

    perf: config add build info

--- src/store/user.ts
+++ src/store/user.ts
@@ -2,12 +2,17 @@ import { defineStore } from "pinia";
 
 export const useUserStore = defineStore({
   id: 'user', // 必填，唯一，此例中为一个user的state集合
-  state: () => {
-    return {
-      name: 'lee'
-    }
+  state: () => ({
+      name: 'lee',
+  }),
+  getters: {
+    name(state) {
+      // do some format or others
+        return state.name;
+    },
   },
   actions: {
+    // change state
     updateName(name: string) {
       this.name = name
     }
--- vite.config.ts
+++ vite.config.ts
@@ -6,17 +6,39 @@ import * as path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
+    // 别名设置
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
   },
   plugins: [vue(), vuejsx()],
+  build: {
+    terserOptions: {
+      compress: {
+        //生产环境时移除console
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    // 取消计算文件大小，加快打包速度
+    reportCompressedSize: false,
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        assetFileNames: '[ext]/[name]-[hash].[ext]',
+      },
+    },
+  },
   server: {
+    // 设置启动端口
     port: 8080,
     hmr: {
       host: '127.0.0.1',
       port: 8080
     },
+    // 设置代理
     proxy: {
       '/api': {
         target: 'https://xunlei.com', // 接口服务地址
